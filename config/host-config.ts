0791b2e858b664de3eaa20215cfa39372ae04ecf
codimiracle <codimiracle@outlook.com>
2020-04-23 01:27:21 +0800

    polish host config in production

--- config/host-config.ts
+++ config/host-config.ts
@@ -1,4 +1,5 @@
 // server side render
 export const FRONTEND_HOSTNAME = "localhost"
 // use auto config
+export const BACKEND_PORT = null
 export const BACKEND_HOSTNAME = "huidu-server.codimiracle.com"
